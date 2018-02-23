8bb1cf726d3231bdef95ef429a1c0e04d3504f65
popovsli <popovsli91@gmail.com>
2018-02-23 11:05:12 +0200

    Add project files.

---
+++ DatastructuresAndAlgorithms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DatastructuresAndAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
